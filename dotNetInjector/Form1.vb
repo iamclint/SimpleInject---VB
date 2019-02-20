670b6365534734c849ac307f5bb155199ae2e203
Clint <clint.ban@gmail.com>
2019-02-20 17:30:04 -0600

    Fixed some listview bugs and browse button failing on x86 due to variable reading the x64 setting

--- dotNetInjector/Form1.vb
+++ dotNetInjector/Form1.vb
@@ -16,7 +16,10 @@ Public Class Form1
     Public Shared Function is32bit(ByVal process_id As Integer) As Boolean
     End Function
     <DllImport("libinj.dll", SetLastError:=True, CallingConvention:=CallingConvention.Cdecl)>
-    Public Shared Function Inject(ByVal process_id As Integer, ByVal dll As String) As Boolean
+    Public Shared Function Inject(ByVal process_id As Integer, ByVal dll As String, ByVal method As Integer, Optional ByVal handle As IntPtr = Nothing) As Boolean
+    End Function
+    <DllImport("libinj.dll", SetLastError:=True, CallingConvention:=CallingConvention.Cdecl)>
+    Public Shared Function getLastError() As String
     End Function
 
     Function filterProcesses(ByVal str As String) As Boolean
@@ -38,9 +41,9 @@ Public Class Form1
 
         'add new processes
         For Each p As Process In Process.GetProcesses()
-            ProcessIdList.Add(p.Id)
-            If Not filterProcesses(p.ProcessName) Or My.Settings.Filters = "" Then
-                If Not Processes.ContainsKey(p.Id) And p.Id <> 0 Then
+            If Not filterProcesses(p.ProcessName) And p.Id > 0 Then
+                ProcessIdList.Add(p.Id) 'keep a list of all process id's so they can be removed when no longer valid
+                If Not Processes.ContainsKey(p.Id) Then
                     Dim cProc As New Proc
                     Dim new_item As New ListViewItem()
                     new_item.Text = p.ProcessName
@@ -53,24 +56,32 @@ Public Class Form1
                     cProc.item = new_item
                     cProc.process = p.MainWindowTitle
                     Processes.Add(p.Id, cProc)
-                Else 'Update the window
-                    If (is32bit(p.Id)) Then
-                        Dim window As String = p.MainWindowTitle.Trim()
-                        Processes.Item(p.Id).item.SubItems(1).Text = window 'update the title
-                    End If
+                Else
+                    Dim window As String = p.MainWindowTitle.Trim()
+                    Processes.Item(p.Id).item.SubItems(1).Text = window 'update the title
                 End If
-            Else
-                Console.WriteLine("Removing " & p.Id)
-                Processes.Remove(p.Id)
-                ProcessIdList.Remove(p.Id)
             End If
         Next
+
+        Dim removeList As List(Of Integer) = New List(Of Integer)
+        For Each process_id As Integer In Processes.Keys
+            If Not ProcessIdList.Contains(process_id) Then 'if the process is still in our local processes dictionary but its no longer a valid process we need to remove it
+                removeList.Add(process_id)
+            End If
+        Next
+
+        For Each process_id As Integer In removeList
+            Console.WriteLine("Removing from dictionary: " & Processes(process_id).process)
+            Processes.Remove(process_id)
+        Next
+
 
         Dim x As Integer
         For Each item As ListViewItem In ProcessList.Items
             Integer.TryParse(item.Name, x)
             If x <> 0 Then
-                If Not ProcessIdList.Contains(x) Then
+                If Not Processes.ContainsKey(x) Then
+                    Console.WriteLine("Removing from listview: " & item.SubItems(0).Text)
                     ProcessList.Items.Remove(item)
                 End If
             End If
@@ -95,7 +106,7 @@ Public Class Form1
         With ld
             .DefaultExt = ".dll"
             .Filter = "Dll Files|*.dll"
-            .InitialDirectory = My.Settings.FileToInject.Substring(0, My.Settings.FileToInject64.LastIndexOf("\"))
+            .InitialDirectory = My.Settings.FileToInject.Substring(0, My.Settings.FileToInject.LastIndexOf("\"))
         End With
         If ld.ShowDialog = Windows.Forms.DialogResult.OK Then
             DllPath.Text = ld.FileName
@@ -107,11 +118,8 @@ Public Class Form1
         refreshProcessList()
     End Sub
 
-    Private Function isValidFile() As Boolean
-        If ProcessList.SelectedItems.Count <= 0 Then
-            Return False
-        End If
-        If Not File.Exists(DllPath.Text) Then
+    Private Function isValidFile(ByVal file_path As String) As Boolean
+        If Not File.Exists(file_path) Then
             Return False
         End If
         Return True
@@ -122,17 +130,32 @@ Public Class Form1
         My.Settings.FileToInject64 = DllPath64.Text
         Dim pID As Integer = Integer.Parse(ProcessList.SelectedItems(0).SubItems(2).Text.ToString)
         Dim title As String
+        Dim balloonText As String
         If (ProcessList.SelectedItems(0).SubItems(3).Text.ToString = "x86") Then
             title = DllPath.Text
-            Inject(pID, DllPath.Text)
+            If (Not isValidFile(DllPath.Text)) Then
+                NotifyMessage.ShowBalloonTip(3000, "Error", "Invalid file selected: " & DllPath.Text, ToolTipIcon.Error)
+                Exit Sub
+            End If
+
+            If Not Inject(pID, DllPath.Text, 1) Then
+                NotifyMessage.ShowBalloonTip(3000, "Error", getLastError(), ToolTipIcon.Error)
+            Else
+                balloonText = title.Substring(title.LastIndexOf("\") + 1, title.Length - title.LastIndexOf("\") - 1) & " -> " & ProcessList.SelectedItems(0).SubItems(0).Text.ToString
+                NotifyMessage.ShowBalloonTip(3000, "Injection Successful!", balloonText, ToolTipIcon.Info)
+            End If
         Else
             title = DllPath64.Text
-            Inject(pID, DllPath64.Text)
-        End If
-        If Marshal.GetLastWin32Error() <> 0 Then
-            NotifyMessage.ShowBalloonTip(3000, "Error", "Injection failed!" & vbCrLf & "Win32 Error #" & Marshal.GetLastWin32Error(), ToolTipIcon.Error)
-        Else
-            NotifyMessage.ShowBalloonTip(3000, "Injection Successful!", title.Substring(DllPath.Text.LastIndexOf("\") + 1, title.Length - title.LastIndexOf("\") - 1) & " -> " & Integer.Parse(ProcessList.SelectedItems(0).SubItems(0).Text.ToString), ToolTipIcon.Info)
+            If (Not isValidFile(DllPath64.Text)) Then
+                NotifyMessage.ShowBalloonTip(3000, "Error", "Invalid file selected: " & DllPath64.Text, ToolTipIcon.Error)
+                Exit Sub
+            End If
+            If Not Inject(pID, DllPath64.Text, 1) Then
+                NotifyMessage.ShowBalloonTip(3000, "Error", getLastError(), ToolTipIcon.Error)
+            Else
+                balloonText = title.Substring(title.LastIndexOf("\") + 1, title.Length - title.LastIndexOf("\") - 1) & " -> " & ProcessList.SelectedItems(0).SubItems(0).Text.ToString
+                NotifyMessage.ShowBalloonTip(3000, "Injection Successful!", balloonText, ToolTipIcon.Info)
+            End If
         End If
     End Sub
 
@@ -141,30 +164,6 @@ Public Class Form1
         Me.Show()
     End Sub
 
-    Private Sub ProcessList_DragDrop(sender As Object, e As DragEventArgs) Handles ProcessList.DragDrop, DllPath.DragDrop
-        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
-            Dim Files() As String
-            Files = e.Data.GetData(DataFormats.FileDrop)
-            If Files.Length > 0 Then
-                DllPath.Text = Files(0).ToString
-            End If
-        End If
-    End Sub
-
-    Private Sub ProcessList_DragEnter(sender As Object, e As DragEventArgs) Handles ProcessList.DragEnter, DllPath.DragEnter
-        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
-            e.Effect = DragDropEffects.All
-        Else
-            e.Effect = DragDropEffects.None
-        End If
-    End Sub
-    Private Sub ProcessList_DragEnter64(sender As Object, e As DragEventArgs) Handles DllPath64.DragEnter
-        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
-            e.Effect = DragDropEffects.All
-        Else
-            e.Effect = DragDropEffects.None
-        End If
-    End Sub
     Private Sub DllBrowse64_Click(sender As Object, e As EventArgs) Handles DllBrowse64.Click
         Dim ld As OpenFileDialog = New System.Windows.Forms.OpenFileDialog
         With ld
